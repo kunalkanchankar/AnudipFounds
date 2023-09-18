468dc559b2d88c8b6738871f0732174fad20b5f5
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-09-18 12:40:27 +0530

    day18092023

--- AnudipFounds/src/day1809/Vowels.java
+++ AnudipFounds/src/day1809/Vowels.java
@@ -4,15 +4,14 @@ public class Vowels {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a character: ");
-        char charValue = scanner.next().charAt(0);
-        
-        if(charValue=='a' || charValue=='e' || charValue=='i' || charValue=='o' || charValue=='u' ) {
+		char charValue = scanner.next().charAt(0);
+		if(charValue=='a' || charValue=='e' || charValue=='i' || charValue=='o' || charValue=='u' ) {
         	System.out.print(charValue+" is a Vowel");	
         }
-        else if(charValue=='a' || charValue=='e' || charValue=='i' || charValue=='o' || charValue=='u') {
+		else if(charValue=='A' || charValue=='E' || charValue=='I' || charValue=='O' || charValue=='U') {
         	System.out.print(charValue+" is a Vowel");
         }
-        else {
+		else {
         	System.out.print(charValue+" is a Consonant");
         }
 	}
