76ddd96411f8ebca7e025ef477fdd0806418aa71
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-09-20 12:08:20 +0530

    day20092023

---
+++ AnudipFounds/src/day2009/Exponent.java
@@ -0,0 +1,18 @@
+package day2009;
+
+import java.util.Scanner;
+public class Exponent {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        
+		System.out.println("Enter a numbers:");
+        int  num1 = scanner.nextInt();
+        System.out.println("Enter power of :");
+        int  num2 = scanner.nextInt();
+        int result = 1;
+        for(int i = 1;i<=num2;i++) {
+        	result = num1 * result;
+        }
+        System.out.println("Result of "+num1+" power of "+num2+" is : "+result);
+	}
+}
