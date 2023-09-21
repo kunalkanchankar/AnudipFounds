ade8e29219856049c4e909d83849956c854f7834
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-09-21 10:28:34 +0530

    day20092023

--- AnudipFounds/src/day2009/StarPattern.java
+++ AnudipFounds/src/day2009/StarPattern.java
@@ -7,17 +7,13 @@ public class StarPattern {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter no of row : ");
-        int rows = scanner.nextInt();
+        int row = scanner.nextInt();
         
-        for (int i=1; i<=rows; i++){ 
-            for (int k=rows; k>=i; k--){
-                System.out.print("* ");
-            }
-            for (int j=1; j<i; j++){
-                System.out.print(" ");
+        for (int i=1; i<=row; i++){ 
+            for (int j=row; j>=i; j--){
+                System.out.print(j+" "); //Put * star for star pattern
             }
             System.out.println();
-            star1();
         }
     }
 }
