2690591071d8b6f177b1d9879b61f4aed0541a39
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-09-27 12:01:32 +0530

    day27092023

--- AnudipFounds/src/day2709/ReverseArray.java
+++ AnudipFounds/src/day2709/ReverseArray.java
@@ -5,17 +5,15 @@ public class ReverseArray {
 	 public static void main(String[] args) {
 	    	int[] marks= {30,24,26,15,12,45,40,65,54};
 	    	int temp = 0;
-	    	for (int i = marks.length; i <= marks.length; i--){  
-	            for (int j = i + 1; j < marks.length; j++) {  
-	                if (marks[i] > marks[j]) {  
-	                    temp = marks[i];  
-	                    marks[i] = marks[j];  
-	                    marks[j] = temp;  
-	                }
-	            }
-	        }
-	    	System.out.println("\nMinimum marks : "+marks[0]);
-	        System.out.println("Maximum marks : "+marks[marks.length-1]);
-	        System.out.println("second Maximum marks : "+marks[marks.length-2]);  
+	    	
+	    	for (int i = 0; i < marks.length/2; i++){  
+	    		temp = marks[marks.length-1-i];
+	    		marks[marks.length-1-i] = marks[i];
+	    		marks[i] = temp;
+	    	}
+	    	System.out.println("Reverse Array : ");
+	    	for(int i = 0;i<marks.length;i++) {
+	    		System.out.print(" "+marks[i]);
+		    }
 	    }
 }
