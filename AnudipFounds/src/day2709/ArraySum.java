a186e1b78877285356a2359e6c7fa1ae457c43fd
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-09-27 10:56:08 +0530

    day270923

--- AnudipFounds/src/day2709/ArraySum.java
+++ AnudipFounds/src/day2709/ArraySum.java
@@ -1,16 +1,14 @@
 package day2709;
 
 public class ArraySum {
+
 	public static void main(String[] args) {
 		int[] marks= {30,24,26,15,12};
-		int temp = 0;
-		
-		System.out.print("Before Swap : \nfirst = "+marks[0]+"\nLast = "+marks[marks.length-1]);
-		temp = marks[0];
-		marks[0] = marks[marks.length-1];
-		marks[marks.length-1] = temp;
-		System.out.print("\nAfter Swap : \nfirst = "+marks[0]+"\nLast = "+marks[marks.length-1]);
-		
-		
+		int sum = 0;
+		for(int i=0;i<marks.length;i++)	{
+			sum = marks[i]+sum;
+			System.out.print(marks[i]+" + " );
+		}
+		System.out.print("0 = "+sum);
 	}
 }
