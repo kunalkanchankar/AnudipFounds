11ff6d3f80d9968e5919cb705122906eb3b5a116
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-09-27 10:57:09 +0530

    day270923

---
+++ AnudipFounds/src/day2709/SwapLastandFirst.java
@@ -0,0 +1,17 @@
+package day2709;
+
+public class SwapLastandFirst {
+
+	public static void main(String[] args) {
+		int[] marks= {30,24,26,15,12};
+		int temp = 0;
+		
+		System.out.print("Before Swap : \nfirst = "+marks[0]+"\nLast = "+marks[marks.length-1]);
+		temp = marks[0];
+		marks[0] = marks[marks.length-1];
+		marks[marks.length-1] = temp;
+		System.out.print("\nAfter Swap : \nfirst = "+marks[0]+"\nLast = "+marks[marks.length-1]);
+
+	}
+
+}
