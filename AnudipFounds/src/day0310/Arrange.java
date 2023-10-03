122c39d433838c46338f1494c51a7efc393b0df6
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-10-03 13:56:27 +0530

    day03102023

--- AnudipFounds/src/day0310/Arrange.java
+++ AnudipFounds/src/day0310/Arrange.java
@@ -12,13 +12,14 @@ public class Arrange {
 		for (int i = 0; i < arr.length; i++){  
             for (int j = i + 1; j < arr.length; j++) {  
             	if(arr[i].charAt(0) < arr[j].charAt(0)) {
+            	System.out.print(1+" "+ arr[i]+" with "+arr[j]+"| ");
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;		
-//				System.out.println(arr[i]+" with "+arr[j]);
+				System.out.println(arr[i]+" -> "+arr[j]);
 			}	
 		}
-		System.out.println(arr[i]);
+//		System.out.println(arr[i]);
 	}
 
 }
