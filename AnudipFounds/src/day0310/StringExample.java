9f8ed82f2b7c10044614029221d6996f3a0b8df5
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-10-03 10:51:20 +0530

    day03102023

---
+++ AnudipFounds/src/day0310/StringExample.java
@@ -0,0 +1,23 @@
+package day0310;
+
+import java.util.Scanner;
+
+public class StringExample {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a character: ");
+		String charValue = scanner.next();
+		if(charValue.charAt(0)=='a' || charValue.charAt(0)=='e' || charValue.charAt(0)=='i' || charValue.charAt(0)=='o' || charValue.charAt(0)=='u' ) {
+        	System.out.print(charValue.charAt(0)+" is a Vowel");	
+        }
+		else if(charValue.charAt(0)=='A' || charValue.charAt(0)=='E' || charValue.charAt(0)=='I' || charValue.charAt(0)=='O' || charValue.charAt(0)=='U') {
+        	System.out.print(charValue.charAt(0)+" is a Vowel");
+        }
+		else {
+        	System.out.print(charValue.charAt(0)+" is a Consonant");
+        }
+		for(int i = 0 ; i < charValue.length(); i++) {
+			System.out.print(" "+charValue.charAt(i));
+		}
+	}
+}
--- AnudipFounds/src/day2909/Enhance.java
+++ AnudipFounds/src/day2909/Enhance.java
@@ -3,14 +3,13 @@ package day2909;
 public class Enhance {
 
 	public static void main(String[] args) {
-		String[][] color= {{"red","blue","black"},{"white","pink","lemon"}};
+		String[][][] color= {{{"red","blue","black"},{"white","pink","lemon"}},{{"red","yellow","green"}}};
 		
-		for(String[] x : color ) {
-			for(String y:x) {
-				System.out.println(y);
+		for(String[][] x : color ) {
+			for(String[] y:x) {
+				for(String z:y)
+				System.out.println(z);
 			}
 		}
- 
 	}
-
 }
