b0adc26fcc7e1cacdcf8a38d91c5f72e6a13470f
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-10-09 12:57:27 +0530

    day09102023

---
+++ AnudipFounds/src/day0910/Inher.java
@@ -0,0 +1,28 @@
+package day0910;
+
+class Human{
+	String name;
+	String aid;
+	public Human(String name, String aid) {
+		this.name = name;
+		this.aid = aid;
+	}
+}
+class Student extends Human{
+	String coursename;
+	int marks; 
+	public Student (String name, String aid, String coursename, int marks) {
+		super(name, aid);
+		this.coursename = coursename;
+		this.marks = marks;
+	}
+	public void displayInfo() {
+		System.out.println("Student name:  "+name+"\nId : "+aid+"\nCourse name : "+coursename+"\nMarks: "+marks);
+	}
+}
+public class Inher {
+	public static void main(String args[]) {
+		Student obj = new Student("kunal","emp001","Adv java in Angular",80);
+		obj.displayInfo();
+	}
+}
