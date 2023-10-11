e2a99fde70128b538e288d4f6b650780ce2eecec
Kunal Kanchankar <49075447+kunalkanchankar@users.noreply.github.com>
2023-10-11 12:31:46 +0530

    day11102023

---
+++ AnudipFounds/src/day1110/Arithmetic.java
@@ -0,0 +1,14 @@
+package day1110;
+
+interface Multiple{
+	public int mul(int a,int b);
+}
+
+public class Arithmetic {
+	public static void main(String args[]) {
+		Multiple mul = (x,y) ->{
+			return x*y;
+		};
+		System.out.println(mul.mul(10,20));
+	}
+}
